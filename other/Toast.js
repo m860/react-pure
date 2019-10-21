05dbef929243fb138ed08e13db0e50a153d65ee4
m860 <jean.ma.1986@gmail.com>
2019-10-21 18:15:07 +0800

    feat(toast): 完成Toast

---
+++ other/Toast.js
@@ -0,0 +1,98 @@
+/**
+ * @flow
+ * @author Jean.h.ma 2019-09-23
+ */
+import * as React from "react"
+import {EventEmitter} from "fbemitter"
+import uuid from "uuid/v1"
+import update from "immutability-helper"
+
+type Props = {}
+
+export default React.memo<Props>(function (props: Props) {
+
+    const [items, setItems] = React.useState<Array<ToastItem & { expire: number }>>([]);
+
+    React.useEffect(() => {
+        const append = (item: ToastItem) => {
+            setItems(update(items, {$push: [{...item, expire: Date.now() + item.timeout}]}));
+        };
+        // $FlowFixMe
+        emitter.addListener(KEY_APPEND_ITEM, append);
+
+        let timer = null;
+        if (items.length > 0) {
+            timer = setInterval(() => {
+                const nextItems = items.filter(f => f.expire >= Date.now());
+                setItems(nextItems);
+            }, 1000);
+        }
+
+        return () => {
+            emitter.removeAllListeners(KEY_APPEND_ITEM);
+            if (timer) {
+                clearInterval(timer);
+            }
+        }
+    }, [items]);
+
+    return (
+        <div className="pure-toast">
+            {items.map((item: ToastItem, index: number) => {
+                return (
+                    <div className={`pure-toast-item pure-toast-item-${item.type}`} key={item.id}>
+                        <span>{item.message}</span>
+                    </div>
+                );
+            })}
+        </div>
+    )
+});
+
+export const ToastItemTypes = {
+    info: "info",
+    warn: "warn",
+    error: "error"
+};
+
+export type ToastItem = {
+    id: string,
+    type: $Values<typeof ToastItemTypes>,
+    message: string,
+    timeout: number
+};
+
+const emitter = new EventEmitter();
+
+const KEY_APPEND_ITEM = "AppendItem";
+
+export function info(message: string, rest: $Shape<ToastItem> = {timeout: 3000}) {
+    const item: ToastItem = {
+        id: uuid(),
+        ...rest,
+        message,
+        type: ToastItemTypes.info,
+
+    };
+    emitter.emit(KEY_APPEND_ITEM, item);
+}
+
+export function warn(message: string, rest: $Shape<ToastItem> = {timeout: 3000}) {
+    const item: ToastItem = {
+        id: uuid(),
+        ...rest,
+        message,
+        type: ToastItemTypes.warn,
+    };
+    emitter.emit(KEY_APPEND_ITEM, item);
+}
+
+export function error(message: string, rest: $Shape<ToastItem> = {timeout: 3000}) {
+    const item: ToastItem = {
+        id: uuid(),
+        ...rest,
+        type: ToastItemTypes.error,
+        message
+    };
+    emitter.emit(KEY_APPEND_ITEM, item);
+}
---
+++ stories/Other.stories.js
@@ -0,0 +1,21 @@
+/**
+ * @flow
+ * @author Jean.h.ma 2019-10-21
+ */
+import * as React from "react"
+import Toast, {error, info, warn} from "../other/Toast";
+
+export default {
+    title: "Other"
+}
+
+export const toast = () => {
+    return (
+        <div>
+            <Toast/>
+            <button type="button" className="pure-button" onClick={() => info(`info:${Date.now()}`)}>info</button>
+            <button type="button" className="pure-button" onClick={() => warn(`info:${Date.now()}`)}>warn</button>
+            <button type="button" className="pure-button" onClick={() => error(`info:${Date.now()}`)}>error</button>
+        </div>
+    )
+};
