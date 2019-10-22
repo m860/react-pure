986235b3729a9be384a631c319b5aed8d90eb4c0
m860 <jean.ma.1986@gmail.com>
2019-10-22 11:00:00 +0800

    chore: 增加storybook参数设置

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,11 @@
-import {configure} from '@storybook/react';
-
+import {configure,addParameters} from '@storybook/react';
 import "../index"
+
+addParameters({
+    options:{
+        name:"React-Pure"
+    }
+})
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
